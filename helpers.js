5580e240e9d1444d2aacdace6fe08e061a46c449
Nikhil-Jr <86511422+Nikhil-Jr@users.noreply.github.com>
2021-07-20 07:43:56 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *ALEENA*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *ALEENA*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *ALEENA*:  ```" + msg + "```"
 }
 
 
