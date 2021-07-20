392c916e17e66987b3726fa60f51fe08d7fef2b4
Nikhil-Jr <86511422+Nikhil-Jr@users.noreply.github.com>
2021-07-20 14:53:38 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -153,7 +153,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '919746138832@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Aleena','Poda','Da','Ayye','aleena','Poda','Sed','va','wow','ayin','back','Back','Bot','fuck','Fuck','Hehe','Hello','Hlo','Kill','kill','kiss','line','love','mwolu','Mwolu','single','tha','thund','z','Z']
+const array = ['Da','Ayye','Poda','Sed','va','wow','ayin','back','Back','Bot','fuck','Fuck','Hehe','Hello','Hlo','Kill','kill','kiss','line','love','mwolu','Mwolu','single','tha','thund','z','Z']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
