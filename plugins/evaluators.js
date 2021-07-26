7e9ab5f6bf22bd07e786ad4d82789757a98f46e2
Nikhil-Jr <86511422+Nikhil-Jr@users.noreply.github.com>
2021-07-26 21:54:51 +0530

    Update evaluators.js

--- plugins/evaluators.js
+++ plugins/evaluators.js
@@ -55,7 +55,7 @@ if (Config.LANG == 'HI') ldc = '*लिंक का पता चला!*'
 if (Config.LANG == 'ES') ldc = '*Enlace Detectado!*'
 Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
     if (antilink_var == 'true' && message.jid !== '905511384572-1616356915@g.us') {
-        let regex1 = new RegExp('http://')
+        let regex1 = new RegExp('http://telegram')
         let regex2 = new RegExp('https://chat')
         let regex3 = new RegExp('https://meet')
         let regex4 = new RegExp('https://t.co')
