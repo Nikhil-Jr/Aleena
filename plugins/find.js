1836dec5f072a0897041029264826ac0b80ef8b4
Nikhil-Jr <86511422+Nikhil-Jr@users.noreply.github.com>
2021-07-28 09:14:49 +0530

    Update find.js

--- plugins/find.js
+++ plugins/find.js
@@ -3,13 +3,17 @@ const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 
 const fs = require('fs');
 const axios = require('axios');
+const conf = require('../config');
 const FormData = require('form-data');
 const ffmpeg = require('fluent-ffmpeg');
 
-const FIND_DESC = "Finds the Song"
+const Language = require('../language');
+const Lang = Language.getString('scrapers');
 
-Asena.addCommand({pattern: 'music', fromMe: false, desc: FIND_DESC }, (async (message, match) => {
-    if (message.reply_message === false) return await message.client.sendMessage(message.jid, '*reply any mp3 audio file*', MessageType.text);
+let td = conf.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({pattern: 'find', fromMe: td, desc: Lang.FINDS_DESC}, (async (message, match) => {
+    if (message.reply_message === false) return await message.client.sendMessage(message.jid, Lang.NEED_FAUDIO, MessageType.text);
     var filePath = await message.client.downloadAndSaveMediaMessage({
         key: {
             remoteJid: message.reply_message.jid,
@@ -30,13 +34,13 @@ Asena.addCommand({pattern: 'music', fromMe: false, desc: FIND_DESC }, (async (me
         await axios.post('https://api.audd.io/', form, configs).then(async (response) => {
             var res = response.data
             if (res === 'success') {
-                await message.client.sendMessage(message.jid, `Title: ${res.title}\nArtist: ${res.artist}`, MessageType.text);
+                await message.client.sendMessage(message.jid, `${Lang.TITTLE_F} : ${res.title}\n${Lang.ARTIST_F} : ${res.artist}`, MessageType.text);
             } else {
-                await message.client.sendMessage(message.jid, '*Song not found...*', MessageType.text);
+                await message.client.sendMessage(message.jid, Lang.NOT_FOUNDFS, MessageType.text);
             }
         }).catch((error) =>  {
             console.log(error);
         });
     });
 
-}));
+})); 
