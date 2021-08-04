8d629b00104ade42dca348da04ef92ede3dd9cfd
Nikhil-Jr <86511422+Nikhil-Jr@users.noreply.github.com>
2021-08-04 20:06:02 +0530

    Update greetings.js

--- plugins/sql/greetings.js
+++ plugins/sql/greetings.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 TOXIC ALIEN.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsAlfa
 */
 
 const config = require('../../config');
